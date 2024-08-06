9fc0a72bde9327bcf86330dbad847cc1a05ef678
mnnptl <manon@studio-novembre.com>
2024-08-06 09:15:18 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+import WorkdaysCalculator from './WorkdaysCalculator';
+
+export default function App() {
+  return (
+    <div className="App">
+      <WorkdaysCalculator />
+    </div>
+  );
+}
